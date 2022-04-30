339d81249856b9976a8d8a32d1968e2ae5e2e7cb
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-04-30 17:44:27 +0530

    Added needed classes for persistent implementation

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -0,0 +1,10 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+
+public class PersistentExpenseManager extends ExpenseManager {
+    @Override
+    public void setup() throws ExpenseManagerException {
+
+    }
+}
