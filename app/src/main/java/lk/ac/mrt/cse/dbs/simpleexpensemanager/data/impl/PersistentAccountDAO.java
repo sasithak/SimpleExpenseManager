6b5b1cf1033d4e7268d1787a70da523a49c5d939
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-05-01 11:31:05 +0530

    Added the database structure for the persistent classes

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentAccountDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentAccountDAO.java
@@ -2,12 +2,41 @@ package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
 
 import java.util.List;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 
-public class PersistentAccountDAO implements AccountDAO {
+public class PersistentAccountDAO extends SQLiteOpenHelper implements AccountDAO {
+    // Database related constants
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "expense_manager.db";
+    private static final String TABLE_NAME = "tbl_account";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_ACCOUNT_NO = "account_no";
+    private static final String COLUMN_BANK_NAME = "bank_name";
+    private static final String COLUMN_ACCOUNT_HOLDER_NAME = "account_holder_name";
+    private static final String COLUMN_BALANCE = "balance";
+
+    // SQL queries
+    private static final String CREATE_TABLE_QUERY =
+            "create table " + TABLE_NAME + " ( " +
+                    COLUMN_ID + " integer primary key autoincrement, " +
+                    COLUMN_ACCOUNT_NO + " text, " +
+                    COLUMN_BANK_NAME + " text, " +
+                    COLUMN_ACCOUNT_HOLDER_NAME + " text, " +
+                    COLUMN_BALANCE + " real " +
+            " )";
+
+    public PersistentAccountDAO(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
     @Override
     public List<String> getAccountNumbersList() {
         return null;
@@ -36,5 +65,23 @@ public class PersistentAccountDAO implements AccountDAO {
     @Override
     public void updateBalance(String accountNo, ExpenseType expenseType, double amount) throws InvalidAccountException {
 
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE_QUERY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public static String getDbName() {
+        return DB_NAME;
+    }
+
+    public static String getColumnAccountNo() {
+        return COLUMN_ACCOUNT_NO;
     }
 }
--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -1,5 +1,10 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
 import java.util.Date;
 import java.util.List;
 
@@ -7,7 +12,36 @@ import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
-public class PersistentTransactionDAO implements TransactionDAO {
+public class PersistentTransactionDAO extends SQLiteOpenHelper implements TransactionDAO {
+    // Database related constants
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "expense_manager.db";
+    private static final String TABLE_NAME = "tbl_transaction";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_ACCOUNT_NO = "account_no";
+    private static final String COLUMN_DATE = "date";
+    private static final String COLUMN_EXPENSE_TYPE = "expense_type";
+    private static final String COLUMN_AMOUNT = "amount";
+
+    // SQL queries
+    private static final String CREATE_TABLE_QUERY =
+            "create table " + TABLE_NAME + " ( " +
+                    COLUMN_ID + " integer primary key autoincrement, " +
+                    COLUMN_ACCOUNT_NO + " text, " +
+                    COLUMN_DATE + " date, " +
+                    COLUMN_EXPENSE_TYPE + " text, " +
+                    COLUMN_AMOUNT + " real " +
+                    " )" +
+            "constraint fk_account " +
+                    "foreign key " + COLUMN_ACCOUNT_NO + " " +
+                    "references " + PersistentAccountDAO.getDbName() + "(" +
+                            PersistentAccountDAO.getColumnAccountNo() +
+                    ")";
+
+    public PersistentTransactionDAO(@Nullable Context context) {
+        super(context, "transaction.db", null, 1);
+    }
+
     @Override
     public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
 
@@ -21,5 +55,15 @@ public class PersistentTransactionDAO implements TransactionDAO {
     @Override
     public List<Transaction> getPaginatedTransactionLogs(int limit) {
         return null;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE_QUERY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }
